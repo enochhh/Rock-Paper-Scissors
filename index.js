8d0a258b22c3a1382f7d064058c11f4c58a8d3d8
enoch harnanto <enochharnantoo@gmail.com>
2022-01-20 21:49:57 -0800

    Added point system and cleaned up conditional statements

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+let playerScore = 0;
+let compScore = 0;
+
+const res = document.getElementById('results');
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+
+rockBtn.addEventListener("click", playRound);
+paperBtn.addEventListener("click", playRound);
+scissorsBtn.addEventListener("click", playRound);
+
 function computerPlay() {
     const weapons = ["rock", "paper", "scissors"];
     const index = Math.floor(Math.random()*weapons.length);
     return weapons[index];
 }
-
-    const res = document.getElementById('results');
-
-    const rockBtn = document.getElementById('rock');
-    const paperBtn = document.getElementById('paper');
-    const scissorsBtn = document.getElementById('scissors');
-
-    rockBtn.addEventListener("click", playRound);
-    paperBtn.addEventListener("click", playRound);
-    scissorsBtn.addEventListener("click", playRound);
 
 function playRound(e) {
     const playerSelect = e.currentTarget.innerText;
@@ -34,17 +36,19 @@ function playRound(e) {
     if(compSelect == playerSelect) {
         res.innerText = `It's a tie, ${playerSelect} and ${compSelect} are the same.`;
     }
-    else if(compSelect == "rock" && playerSelect == "paper")  {
-        res.innerText = `You win, ${playerSelect} beats ${compSelect}!`;
+
+    else if(
+        (compSelect == "rock" && playerSelect == "paper") || (compSelect == "paper" && playerSelect == "scissors") || (compSelect == "scissors" && playerSelect == "rock"))  {
+            playerScore++;
+            res.innerText = `You win, ${playerSelect} beats ${compSelect}! You have
+            ${playerScore} points and your opponent has ${compScore} points`;
     }
-    else if(compSelect == "paper" && playerSelect == "scissors") {
-        res.innerText = `You win, ${playerSelect} beats ${compSelect}!`;
-    }
-    else if(compSelect == "scissors" && playerSelect == "rock") {
-        res.innerText = `You win, ${playerSelect} beats ${compSelect}!`;
-    }
-    else {
-        res.innerText = `You lose, ${compSelect} beats ${playerSelect}!`;
+
+    else if(
+        (playerSelect == "rock" && compSelect == "paper") || (playerSelect == "paper" && compSelect == "scissors") || (playerSelect == "scissors" && compSelect == "rock"))  {
+            compScore++;
+            res.innerText = `You lose, ${compSelect} beats ${playerSelect}! You have
+            ${playerScore} points and your opponent has ${compScore} points`;
     }
 }
 
@@ -69,6 +73,17 @@ function playRound(e) {
 //     }
 // } 
 
+function endGame() {
+    if (playerScore == 5 || compScore == 5) {
+        if (playerScore == 5) {
+            res.innerText = "You win the game!"
+        }
+
+        else if (compScore == 5) {
+            res.innerText = "Aw, you lost!"
+        }
+    }
+} 
 
 
 
